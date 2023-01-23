5884e0abb088ef8e7a825fc4730c28dbc4237cec
tukky-14 <bigoonage0104@gmail.com>
2023-01-23 22:09:05 +0900

    ランダム名言ページを更新

---
+++ src/pages/quote.tsx
@@ -0,0 +1,71 @@
+import { Button, Card, CardContent } from '@mui/material';
+import { Amplify, Auth, API } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import MainContents from '../components/MainContents';
+import PrivateRoute from '../components/PrivateRoute';
+
+Amplify.configure({
+    API: {
+        endpoints: [
+            {
+                name: 'dev',
+                endpoint: process.env.NEXT_PUBLIC_API_ENDPOINT_QUOTES,
+            },
+        ],
+    },
+});
+
+export default function Quote() {
+    const [quote, setQuote] = useState('');
+    const [quoteList, setQuoteList] = useState([]);
+
+    useEffect(() => {
+        executeGetRollcall();
+    }, []);
+
+    const handleButtonClick = () => {
+        const randamIndex = Math.floor(Math.random() * quoteList.length);
+        setQuote(quoteList[randamIndex]);
+    };
+
+    const executeGetRollcall = async () => {
+        try {
+            const user = await Auth.currentAuthenticatedUser();
+            const token = user.signInUserSession.idToken.jwtToken;
+
+            const getApiInit = {
+                headers: {
+                    Authorization: token,
+                },
+                // queryStringParameters: {
+                // },
+            };
+            const res = await API.get('dev', '/quotes', getApiInit);
+            const quotes = res.map((data: any) => data.quote);
+            setQuoteList(quotes);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <PrivateRoute>
+            <Header />
+            <MainContents title="ランダム名言">
+                <div className="w-2/3 my-4">
+                    <Card variant="outlined">
+                        <div className="h-80 flex flex-col justify-center items-center">
+                            <p className="text-xl font-serif">{quote}</p>
+                        </div>
+                    </Card>
+                </div>
+                <Button variant="contained" onClick={handleButtonClick}>
+                    表示
+                </Button>
+            </MainContents>
+            <Footer />
+        </PrivateRoute>
+    );
+}
