31f80806b78e5edc0c6beec300ede79523353392
tukky-14 <bigoonage0104@gmail.com>
2023-01-31 21:23:09 +0900

    Recoilを使用してグローバルな値を管理するよう更新

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,26 +1,26 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { sidebarOpenState } from '../globalStates/sidebarAtom';
 import { SidebarData } from './SidebarData';
 
 const Sidebar = () => {
-    const [openSidebar, setOpenSideber] = useState(true);
-    const [sidebarSize, setSidebarSize] = useState('w-1/6');
+    const [sideberOpen, setSidebarOpen] = useRecoilState(sidebarOpenState);
 
     const handleMenuIconClick = () => {
-        setOpenSideber(!openSidebar);
-        setSidebarSize(!openSidebar ? 'w-1/6' : 'w-14');
+        setSidebarOpen(!sideberOpen);
     };
 
     return (
-        <aside className={`${sidebarSize} bg-blue-100 flex flex-col`}>
+        <aside className={`${sideberOpen ? 'w-1/6' : 'w-14'} bg-blue-100 flex flex-col`}>
             <button className="hidden sm:inline py-2 pl-4 text-left hover:bg-white" onClick={handleMenuIconClick}>
                 <MenuIcon />
             </button>
             {SidebarData.map((data: { title: string; icon: any; link: string }, index: number) => (
                 <Link href={data.link} className="py-2 pl-4 flex items-center hover:bg-white" key={index}>
                     {data.icon}
-                    {openSidebar && <span className="hidden sm:inline pl-3">{data.title}</span>}
+                    {sideberOpen && <span className="hidden sm:inline pl-3">{data.title}</span>}
                 </Link>
             ))}
         </aside>
---
+++ src/globalStates/sidebarAtom.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const sidebarOpenState = atom({
+    key: 'sidebarOpenState',
+    default: true,
+});
