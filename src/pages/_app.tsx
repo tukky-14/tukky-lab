f611b2eb16110ec7622266db83a2a674830d83fd
tukky-14 <bigoonage0104@gmail.com>
2024-01-25 20:01:10 +0900

    Headタグの変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 import Container from '../components/Container';
 import { ProvideAuth } from '../hooks/useAuth';
@@ -9,29 +8,14 @@ import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
-            <Head>
-                <title>Tukky Lab</title>
-                <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content="エンジニアつっきーの実験のためのWebアプリケーション" />
-                <meta property="og:title" content="Tukky Lab" />
-                <meta property="og:description" content="エンジニアつっきーの実験のためのWebアプリケーション" />
-                <meta property="og:type" content="website" />
-                <meta property="og:image" content="/tukkylab.png" />
-                <meta property="og:url" content="https://tukky-lab.vercel.app/" />
-                <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta name="mobile-web-app-capable" content="yes" />
-            </Head>
-
-            <RecoilRoot>
-                <ProvideAuth>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <Container>
-                            <Component {...pageProps} />
-                        </Container>
-                    </LocalizationProvider>
-                </ProvideAuth>
-            </RecoilRoot>
-        </>
+        <RecoilRoot>
+            <ProvideAuth>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Container>
+                        <Component {...pageProps} />
+                    </Container>
+                </LocalizationProvider>
+            </ProvideAuth>
+        </RecoilRoot>
     );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="ja">
+            <Head>
+                <title>Tukky Lab</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="エンジニアつっきーの実験のためのWebアプリケーション" />
+                <meta property="og:title" content="Tukky Lab" />
+                <meta property="og:description" content="エンジニアつっきーの実験のためのWebアプリケーション" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="/tukkylab.png" />
+                <meta property="og:url" content="https://tukky-lab.vercel.app/" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="mobile-web-app-capable" content="yes" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
